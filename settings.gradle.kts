2415818c82f2274426454c7d9401b268ad60749f
putridea4 <aprilianaputridea@gmail.com>
2020-08-07 01:21:18 -0700

    update pertama

---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "Fundamental Part 1"
+
---
+++ src/main/kotlin/Hello Kotlin.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val nama = "Putri Dea Apriliana"
+    print("Nama saya ")
+    println(nama)
+    val Hobby = "Main Game"
+    print("Hobby saya ")
+    println(Hobby)
+}
